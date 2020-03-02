c037adb81f4ff467e6bb15e0ac0c917fdea2fd31
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-02 15:32:43 -0300

    application pages

---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+function Main(){
+    return (
+        <Title error={false}>
+            Main
+            <small>menor</small>
+        </Title>
+    );
+};
+
+export default Main;
